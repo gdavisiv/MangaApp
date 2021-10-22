bf75e070a0f7fc96ffec70940c8a27043df9d88a
George Davis IV <gdavisiv@gmail.com>
2021-10-22 18:45:03 -0400

    Initial Commit

---
+++ MangaApp/MangaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaAppApp.swift
+//  MangaApp
+//
+//  Created by George Davis IV on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MangaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
