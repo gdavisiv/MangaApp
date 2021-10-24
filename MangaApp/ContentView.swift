123905098a0f86db4fac3f78a5d8729d1c5e4443
George Davis IV <gdavisiv@gmail.com>
2021-10-24 16:24:20 -0400

    Update: Adding Illustration Elements to first Card

--- MangaApp/ContentView.swift
+++ MangaApp/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 4.0){
+        VStack(alignment: .leading, spacing: 4.0) {
+            Image("Illustration 1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             Text("Manga Shop!")
                 .fontWeight(.bold)
             // "/n" adds a nice break see sentence below
             Text("Your place to read/share and \nEarn money from your comics!")
                 .font(.footnote)
         }
+        .padding(.all)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
 
