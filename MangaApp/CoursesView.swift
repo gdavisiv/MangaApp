2524858b1916d98a5e3e525330bca3fbf3614382
George Davis IV <gdavisiv@gmail.com>
2021-10-26 20:27:57 -0400

    Update: Creating/Testing Integration between MACOS and iOS devices

--- MangaApp/CoursesView.swift
+++ MangaApp/CoursesView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct CoursesView: View {
+    //Needed to run on iOS and MAC
+    @ViewBuilder
     var body: some View {
+        #if os(iOS)
+        content
+            .listStyle(InsetGroupedListStyle())
+        #else
+        content
+        #endif
+    }
+    var content: some View {
         List(0 ..< 20) { item in
             CourseRow()
         }
         //Allows the list style to be inset so that its seperated from the top and edges
-        .listStyle(InsetGroupedListStyle())
         .navigationTitle("Most Popular Anime & Manga")
     }
 }
