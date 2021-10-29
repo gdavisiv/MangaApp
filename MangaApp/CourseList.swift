4def69ac303e1f239fd3a9bae4a17f5a04b2d933
George Davis IV <gdavisiv@gmail.com>
2021-10-28 20:53:20 -0400

    Update: Completed Updates and creation of new CourseView to better organize file structure

---
+++ MangaApp/CourseList.swift
@@ -0,0 +1,35 @@
+//
+//  CourseList.swift
+//  MangaApp
+//
+//  Created by George Davis IV on 10/28/21.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    //Needed to run on iOS and MAC
+    @ViewBuilder
+    var body: some View {
+        #if os(iOS)
+        content
+            .listStyle(InsetGroupedListStyle())
+        #else
+        content
+            .frame(minWidth: 800, minHeight: 600)
+        #endif
+    }
+    var content: some View {
+        List(0 ..< 20) { item in
+            CourseRow()
+        }
+        //Allows the list style to be inset so that its seperated from the top and edges
+        .navigationTitle("Most Popular Anime & Manga")
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
+}
--- MangaApp/CoursesView.swift
+++ MangaApp/CoursesView.swift
@@ -9,23 +9,8 @@
 import SwiftUI
 
 struct CoursesView: View {
-    //Needed to run on iOS and MAC
-    @ViewBuilder
     var body: some View {
-        #if os(iOS)
-        content
-            .listStyle(InsetGroupedListStyle())
-        #else
-        content
-            .frame(minWidth: 800, minHeight: 600)
-        #endif
-    }
-    var content: some View {
-        List(0 ..< 20) { item in
-            CourseRow()
-        }
-        //Allows the list style to be inset so that its seperated from the top and edges
-        .navigationTitle("Most Popular Anime & Manga")
+        Text("")
     }
 }
 
